c93a8b68c289d1fac84b36832a30ff1e6f244502
LifesGoood <81054425+LifesGoood@users.noreply.github.com>
2021-12-30 16:41:07 +0100

    Update main.c

--- Lab 7/Lab7_Vezba2/main.c
+++ Lab 7/Lab7_Vezba2/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int elka(int x);
 int main()
 {
     int m,x;
     scanf("%d",&x);
     x=x/2+1;
 
-    for(int i=1;i<=x;i++){
+    elka(x);
+    return 0;
+}
+int elka(int x){
+ for(int i=1;i<=x;i++){
         for(int j=1;j<=x-i;j++){
             printf(" ");
         }
@@ -18,5 +22,4 @@ int main()
 
         printf("\n");
     }
-    return 0;
 }
